1920d7596f105f640af42191ba3eadee35fa8d46
d <putgeminmouth@gmail.com>
2016-04-05 19:30:59 -0400

    Add simple name based scoring.

--- app/dao/Dao.scala
+++ app/dao/Dao.scala
@@ -2,7 +2,8 @@ package dao
 
 import scala.io.Source
 
-case class Suggestion(name: String, latitude: String, longitude: String, score: Int)
+case class Coordinate(latitude: Double, longitude: Double)
+case class Suggestion(name: String, latitude: String, longitude: String, score: Double)
 
 object Dao {
 
@@ -14,29 +15,49 @@ object Dao {
         }
     }
 
+    type Row = Map[String, String]
+    type ScoredRow = (Double, Row)
+
     private lazy val rawData = Source.fromFile("data/cities_canada-usa.tsv")
                       .getLines
                       .map(_.split("\\t").toSeq)
                       .toSeq
-                      .map( row => row.map( cell => cell.toLowerCase ) ) // TODO: temp simplification
+    lazy val columnNameByIndex = rawData.head
     lazy val columnNameToIndex = rawData.head.zipWithIndex.toMap
 
     lazy val rowData = rawData.drop(1)
-    def col(name: String)(implicit row: Seq[String]) = columnNameToIndex.get(name).map(row).get
+                              // make rows associative instead of index based
+                              .map{ row => row.zipWithIndex.map(z => columnNameByIndex(z._2) -> z._1).toMap }
 
-    def mapSuggestion(row: Seq[String]) = {
-        implicit val r = row
-        Suggestion(col("name"), col("lat"), col("long"), 0)
+    def mapSuggestion(t: ScoredRow) = {
+        val row = t._2
+        Suggestion(row("name"), row("lat"), row("long"), t._1)
     }
 }
 
 class Dao {
     import Dao._
-    def selectByNameStart(namePrefix: String) = {
-        rowData.filter{ implicit row =>
-            col("name").startsWith(namePrefix)
-        }.map(mapSuggestion)
-        // TODO: score
-         .sortWith( (a, b) => b.score > a.score )
+
+    def selectByNameStart(namePrefix: String, coordinates: Option[Coordinate] = None) = {
+        val map = (r: Seq[ScoredRow]) => r.map(mapSuggestion)
+
+        val scoreByName = (results: Seq[Row]) => {
+            val filtered = results.filter(_("name").toLowerCase.startsWith(namePrefix))
+            filtered
+                   .sortBy(_("name"))
+                   .zipWithIndex
+                   .map { case (result, index) =>
+                val score = 1 - (index / (filtered.length-1).toDouble)
+                score -> result
+            }
+        }
+
+        val scoreByCoordinates = identity[Seq[ScoredRow]] _ // TODO: score
+
+        val sort = (r: Seq[Suggestion]) => r.sortWith( (a, b) => a.score > b.score )
+
+        val search = scoreByName andThen scoreByCoordinates andThen map andThen sort
+
+        search(rowData)
     }
 }
